666b0d2a6fee6dbac165e7544db26d861f36cece
Hyunbin Lee <46880076+jshyunbin@users.noreply.github.com>
2019-12-29 00:42:34 +0900

    added theme typography

---
+++ lib/ui/theme/style.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+
+class Type {
+  static const TextStyle header1 = TextStyle(
+    fontSize: 96,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w300,
+  );
+  static const TextStyle header2 = TextStyle(
+    fontSize: 60,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w600,
+  );
+  static const TextStyle header3 = TextStyle(
+    fontSize: 48,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle header4 = TextStyle(
+    fontSize: 34,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle header5 = TextStyle(
+    fontSize: 24,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w700,
+  );
+  static const TextStyle header6 = TextStyle(
+    fontSize: 20,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle subtitle1 = TextStyle(
+    fontSize: 16,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle subtitle2 = TextStyle(
+    fontSize: 14,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle body1 = TextStyle(
+    fontSize: 18,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle body2 = TextStyle(
+    fontSize: 14,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle button = TextStyle(
+    fontSize: 14,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w500,
+  );
+  static const TextStyle caption = TextStyle(
+    fontSize: 12,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w400,
+  );
+  static const TextStyle overline = TextStyle(
+    fontSize: 12,
+    fontFamily: 'WorkSans',
+    fontWeight: FontWeight.w600,
+  );
+}
