d76d5652f1b3e92bae9aa4bab464da09efc599a1
buttercrab <jaeyong0201@gmail.com>
2020-01-04 00:10:34 +0900

    edited sql_handler.dart
    - changed structure

--- lib/backend/sql_handler.dart
+++ lib/backend/sql_handler.dart
@@ -3,23 +3,25 @@ import 'dart:async';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 
+enum Database { project, ssh, setting }
+
 class Project {
+  final int id;
+  final String name;
+  final String description;
+  final String initDate;
+  final String modDate;
+  final String localDir;
+  final String sshName;
+
   Project(
       {this.id,
       this.name,
       this.description,
       this.initDate,
-      this.modiDate,
+      this.modDate,
       this.localDir,
       this.sshName});
-
-  final int id;
-  final String name;
-  final String description;
-  final String initDate;
-  final String modiDate;
-  final String localDir;
-  final String sshName;
 
   Map<String, dynamic> toMap() {
     return {
@@ -27,7 +29,7 @@ class Project {
       'name': name,
       'description': description,
       'initDate': initDate,
-      'modiDate': modiDate,
+      'modDate': modDate,
       'localDir': localDir,
       'sshName': sshName,
     };
@@ -35,52 +37,84 @@ class Project {
 }
 
 class SSH {
-  SSH({this.id, this.name, this.ip, this.username, this.password});
-
   final int id;
   final String name;
-  final String ip;
+  final String host;
   final String username;
   final String password;
+
+  SSH({this.id, this.name, this.host, this.username, this.password});
 
   Map<String, dynamic> toMap() {
     return {
       'id': id,
       'name': name,
-      'ip': ip,
+      'ip': host,
       'username': username,
       'password': password,
     };
   }
 }
 
+class Setting {
+  final int id;
+  final String key;
+  final String value;
+
+  Setting({this.id, this.key, this.value});
+
+  Map<String, dynamic> toMap() {
+    return {'id': id, 'key': key, 'value': value};
+  }
+}
+
 class DatabaseHandler {
-  DatabaseHandler() {
+  var _db;
+  Database _type;
+
+  DatabaseHandler(this._type) {
     init();
   }
 
-  var _projectDatabase;
-  var _sshDatabase;
-
-  static const DATATYPE_PROJECT = 1;
-  static const DATATYPE_SSH = 2;
-
   Future<void> init() async {
-    _projectDatabase = openDatabase(
-        join(await getDatabasesPath(), 'project.db'), onCreate: (db, version) {
-      return db.execute(
-          'CREATE TABLE project(id INTEGER PRIMARY KEY UNIQUE NOT NULL '
-          'AUTOINCREMENT, name TEXT NOT NULL UNIQUE, description TEXT, '
-          'initDate TEXT NOT NULL, modiDate TEXT, NOT NULL'
-          'localDir TEXT NOT NULL, sshName TEXT)');
-    }, version: 1);
-    _sshDatabase = openDatabase(join(await getDatabasesPath(), 'ssh.db'),
-        onCreate: (db, version) {
-      return db
-          .execute('CREATE TABLE ssh(id INTEGER PRIMARY KEY UNIQUE NOT NULL '
-              'AUTOINCREMENT, name TEXT NOT NULL UNIQUE, ip TEXT NOT NULL,'
-              ' username TEXT NOT NULL, passwor TEXT NOT NULL)');
-    }, version: 1);
+    switch (_type) {
+      case Database.project:
+        _db = openDatabase(join(await getDatabasesPath(), 'mobIDE_database.db'),
+            onCreate: (db, version) {
+              return db.execute('CREATE TABLE project('
+                  'id INTEGER PRIMARY KEY UNIQUE NOT NULL AUTOINCREMENT, '
+                  'name TEXT NOT NULL UNIQUE, '
+                  'description TEXT, '
+                  'initDate TEXT NOT NULL, '
+                  'modDate TEXT NOT NULL,'
+                  'localDir TEXT NOT NULL, '
+                  'sshName TEXT'
+                  ')');
+            }, version: 1);
+        break;
+      case Database.ssh:
+        _db = openDatabase(join(await getDatabasesPath(), 'mobIDE_database.db'),
+            onCreate: (db, version) {
+              return db.execute('CREATE TABLE ssh('
+                  'id INTEGER PRIMARY KEY UNIQUE NOT NULL AUTOINCREMENT, '
+                  'name TEXT NOT NULL UNIQUE, '
+                  'ip TEXT NOT NULL, '
+                  'username TEXT NOT NULL, '
+                  'password TEXT NOT NULL'
+                  ')');
+            }, version: 1);
+        break;
+      case Database.setting:
+        _db = openDatabase(join(await getDatabasesPath(), 'mobIDE_database.db'),
+            onCreate: (db, version) {
+              return db.execute('CREATE TABLE setting('
+                  'id INTEGER PRIMARY KEY UNIQUE NOT NULL AUTOINCREMENT, '
+                  'key TEXT NOT NULL UNIQUE, '
+                  'value TEXT NOT NULL, '
+                  ')');
+            }, version: 1);
+        break;
+    }
   }
 
 //  Future<void> insert(int dataType, data) async {
@@ -92,3 +126,7 @@ class DatabaseHandler {
 //    }
 //  }
 }
+
+var project = DatabaseHandler(Database.project);
+var ssh = DatabaseHandler(Database.ssh);
+var setting = DatabaseHandler(Database.setting);
