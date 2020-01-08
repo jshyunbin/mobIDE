2b6c84ed7d3866cee7d83bcd805872c59d817885
Hyunbin Lee <46880076+jshyunbin@users.noreply.github.com>
2020-01-09 00:17:29 +0900

    improvements on terminal panel

--- lib/ui/file_edit_page.dart
+++ lib/ui/file_edit_page.dart
@@ -12,6 +12,158 @@ class FileEditPage extends StatelessWidget {
 
   PanelController _pc = new PanelController();
 
+  Widget _panel() {
+    var terminalNum = 1;
+
+    Widget _chip(
+      label,
+      int tabN,
+    ) {
+      return Chip(
+        avatar: CircleAvatar(
+          backgroundColor: Colors.grey.shade500,
+          child: Text(
+            tabN.toString(),
+            style: Type.header5,
+          ),
+        ),
+        label: label,
+        deleteIcon: Icon(Icons.close),
+        onDeleted: () {
+          terminalNum--;
+        },
+      );
+    }
+
+    return Padding(
+      padding: EdgeInsets.fromLTRB(30.0, 12.0, 30.0, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 30,
+                height: 5,
+                decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.all(Radius.circular(12.0))),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 24.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: Text(
+                  'Terminal',
+                  style: Type.header4.apply(color: Colors.black),
+                ),
+              ),
+              for (var i = 0; i < terminalNum; i++)
+                _chip(
+                    Text(
+                      'local',
+                      style: Type.header6,
+                    ),
+                    1),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _body(context) {
+    return CustomScrollView(
+      slivers: <Widget>[
+        SliverAppBar(
+          expandedHeight: 100.0,
+          backgroundColor: Colors.white,
+          pinned: true,
+          floating: true,
+          snap: true,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios,
+              color: Colors.black,
+            ),
+            onPressed: () => Navigator.pop(context),
+          ),
+          title: Text(
+            'test.cpp',
+            style: Type.header4.apply(color: Colors.black),
+          ),
+          centerTitle: true,
+          actions: <Widget>[
+            IconButton(
+                icon: Icon(
+              Icons.play_arrow,
+              color: Colors.green,
+            )),
+            IconButton(
+              icon: Icon(
+                Icons.attach_money,
+                color: Colors.green,
+              ),
+              onPressed: _pc.open,
+            ),
+            IconButton(
+              icon: Icon(
+                Icons.menu,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                _scaffoldKey.currentState.openEndDrawer();
+              },
+            ),
+          ],
+          flexibleSpace: FlexibleSpaceBar(
+            background: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 90,
+                ),
+                Row(
+                  children: <Widget>[
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Text(
+                      'sample0',
+                      style: Type.body1,
+                    ),
+                    Icon(Icons.chevron_right),
+                    Text(
+                      'test.cpp',
+                      style: Type.body1,
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) {
+              return DecoratedBox(
+                child: Text('This is the edit Text property.'),
+                decoration: BoxDecoration(color: Colors.amber),
+              );
+            },
+            childCount: 100,
+          ),
+        )
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,104 +173,10 @@ class FileEditPage extends StatelessWidget {
         borderRadius: panelRadius,
         controller: _pc,
         minHeight: 0,
-        panel: Center(child: Text('Terminal')),
-        body: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              expandedHeight: 100.0,
-              backgroundColor: Colors.white,
-              pinned: true,
-              floating: true,
-              snap: true,
-              leading: IconButton(
-                icon: Icon(
-                  Icons.arrow_back_ios,
-                  color: Colors.black,
-                ),
-                onPressed: () => Navigator.pop(context),
-              ),
-              title: Text(
-                'test.cpp',
-                style: Type.header4.apply(color: Colors.black),
-              ),
-              centerTitle: true,
-              actions: <Widget>[
-                IconButton(
-                    icon: Icon(
-                  Icons.play_arrow,
-                  color: Colors.green,
-                )),
-                IconButton(
-                  icon: Icon(
-                    Icons.attach_money,
-                    color: Colors.green,
-                  ),
-                  onPressed: _pc.open,
-                ),
-                IconButton(
-                    icon: Icon(
-                  Icons.call_received,
-                  color: Colors.blue,
-                )),
-                IconButton(
-                    icon: Icon(
-                  Icons.call_made,
-                  color: Colors.blue,
-                )),
-                IconButton(
-                    icon: Icon(
-                  Icons.call_split,
-                  color: Colors.blue,
-                )),
-                IconButton(
-                  icon: Icon(
-                    Icons.menu,
-                    color: Colors.black,
-                  ),
-                  onPressed: () {
-                    _scaffoldKey.currentState.openEndDrawer();
-                  },
-                ),
-              ],
-              flexibleSpace: FlexibleSpaceBar(
-                background: Column(
-                  children: <Widget>[
-                    SizedBox(
-                      height: 90,
-                    ),
-                    Row(
-                      children: <Widget>[
-                        SizedBox(
-                          width: 20,
-                        ),
-                        Text(
-                          'sample0',
-                          style: Type.body1,
-                        ),
-                        Icon(Icons.chevron_right),
-                        Text(
-                          'test.cpp',
-                          style: Type.body1,
-                        ),
-                      ],
-                    )
-                  ],
-                ),
-              ),
-            ),
-            SliverList(
-              delegate: SliverChildBuilderDelegate(
-                (context, index) {
-                  return DecoratedBox(
-                    child: Text('This is the edit Text property.'),
-                    decoration: BoxDecoration(color: Colors.amber),
-                  );
-                },
-                childCount: 100,
-              ),
-            )
-          ],
-        ),
+        maxHeight: MediaQuery.of(context).size.height -
+            MediaQuery.of(context).padding.top,
+        panel: _panel(),
+        body: _body(context),
       ),
       endDrawer: Drawer(
         child: ListView(
@@ -134,6 +192,19 @@ class FileEditPage extends StatelessWidget {
               leading: Icon(Icons.timeline),
               title: Text('Git: master'),
             ),
+            ListTile(
+              leading: Icon(Icons.call_received),
+              title: Text('Git pull'),
+            ),
+            ListTile(
+              leading: Icon(Icons.send),
+              title: Text('Git commit'),
+            ),
+            ListTile(
+              leading: Icon(Icons.call_made),
+              title: Text('Git push'),
+            ),
+
             Divider(),
             ListTile(
               title: Text('Current Tabs'),
