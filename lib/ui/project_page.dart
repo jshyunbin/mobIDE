f826015a87ff65ad714dffeb56c073b88d37e467
Hyunbin Lee <46880076+jshyunbin@users.noreply.github.com>
2020-01-19 01:21:24 +0900

    rebuilding project_page.dart

--- lib/ui/project_page.dart
+++ lib/ui/project_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:mobide/ui/file_edit_page.dart';
+import 'package:mobide/ui/theme/style.dart';
 
 class ProjectContent {
   ProjectContent({this.title,
@@ -22,8 +23,105 @@ class ProjectPage extends StatelessWidget {
 
   final ProjectContent projectContent;
 
+  //TODO: make processCard an independent class
+  Widget processCard(String title) {
+    return DecoratedBox(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12.0),
+        color: Colors.black54,
+      ),
+      child: Padding(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  title,
+                  style: Type.header5.apply(color: Colors.white),
+                ),
+                Icon(
+                  Icons.keyboard_arrow_up,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+            SizedBox(height: 15.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Icon(
+                  Icons.check_circle,
+                  color: Colors.green,
+                  size: 40,
+                ),
+                SizedBox(width: 10.0),
+                Text('Connected',
+                    style: Type.header6.apply(color: Colors.white))
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          leading: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          title: Text(
+            projectContent.title,
+            style: Type.header4.apply(color: Colors.black),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        body: Padding(
+          padding: EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Text(
+                projectContent.description,
+                style: Type.body1,
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              LayoutBuilder(
+                builder: (context, constraint) {
+                  if (constraint.maxWidth > 600) {
+                    return Row(
+                      children: <Widget>[
+                        Expanded(child: processCard('SSH')),
+                        SizedBox(width: 20.0),
+                        Expanded(child: processCard('Git')),
+                      ],
+                    );
+                  } else {
+                    return Column(
+                      children: <Widget>[
+                        processCard('SSH'),
+                        SizedBox(height: 20.0),
+                        processCard('Git'),
+                      ],
+                    );
+                  }
+                },
+              ),
+              SizedBox(height: 20.0),
+            ],
+          ),
+        ));
+  }
+
+  Widget _temp(context) {
     return Scaffold(
       body: Container(
         padding: const EdgeInsets.all(20),
