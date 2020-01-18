9051d0215e4345dda7cefd4c97b1747eb9800009
Hyunbin Lee <46880076+jshyunbin@users.noreply.github.com>
2020-01-19 00:02:45 +0900

    fixed sliver app bar bugs

--- lib/ui/main_page.dart
+++ lib/ui/main_page.dart
@@ -5,15 +5,6 @@ import 'package:mobide/ui/settings_page.dart';
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
@@ -23,9 +14,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _selectedIndex = 0;
 
-  //TODO: replace the Text classes to each page widgets.
   static List<Widget> _pageWidgets = <Widget>[
-    //TODO: set toDestination parameter
     HomePage(),
     Text('Index 1: SSH'),
     SettingsPage(),
@@ -40,16 +29,8 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: _pageWidgets.elementAt(_selectedIndex),
       ),
       bottomNavigationBar: BottomNavigationBar(
