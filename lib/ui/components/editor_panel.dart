ad7b4416f041ee9befded92c662f8a8619017260
Hyunbin Lee <46880076+jshyunbin@users.noreply.github.com>
2020-03-27 13:12:57 +0900

    added sftp ui in editor_panel.dart

--- lib/ui/components/editor_panel.dart
+++ lib/ui/components/editor_panel.dart
@@ -11,6 +11,91 @@
 //WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 //See the License for the specific language governing permissions and
 //limitations under the License.
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:mobide/ui/components/sftp.dart';
+import 'package:mobide/ui/terminal.dart';
+import 'package:mobide/ui/theme/style.dart';
 
-class EditorPanel extends StatefulWidget {}
+class EditorPanel extends StatefulWidget {
+  EditorPanel({Key key}) : super(key: key);
+
+  @override
+  _EditorPanelState createState() => _EditorPanelState();
+}
+
+class _EditorPanelState extends State<EditorPanel> {
+  int tabValue = 1;
+
+  List<Widget> title = [
+    Text(
+      'Terminal',
+      style: Type.header4.apply(color: Colors.black),
+    ),
+    Text(
+      'SFTP',
+      style: Type.header4.apply(color: Colors.black),
+    ),
+  ];
+
+  List<Widget> content = [
+    TerminalPage(),
+    SFTPPage(),
+  ];
+
+  void _onTap(int newValue) {
+    setState(() {
+      tabValue = newValue;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.fromLTRB(15.0, 12.0, 15.0, 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 35,
+                height: 5,
+                decoration: BoxDecoration(
+                    color: Colors.grey[300],
+                    borderRadius: BorderRadius.all(Radius.circular(12.0))),
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 20.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Expanded(
+                child: title[tabValue],
+              ),
+              CupertinoSlidingSegmentedControl(
+                children: {
+                  0: Text('Terminal'),
+                  1: Text('SFTP'),
+                },
+                groupValue: tabValue,
+                onValueChanged: (newValue) => _onTap(newValue),
+              )
+            ],
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          Expanded(
+            child: content[tabValue],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/file_edit_page.dart
+++ lib/ui/file_edit_page.dart
@@ -13,7 +13,7 @@
 //limitations under the License.
 import 'package:flutter/material.dart';
 import 'package:mobide/backend/file_system.dart';
-import 'package:mobide/ui/terminal.dart';
+import 'package:mobide/ui/components/editor_panel.dart';
 import 'package:mobide/ui/text_editor.dart';
 import 'package:mobide/ui/theme/style.dart';
 import 'package:sliding_up_panel/sliding_up_panel.dart';
@@ -36,51 +36,6 @@ class FileEditPage extends StatelessWidget {
     if (this.file == null) {
       this.file = SSHFile("hello.txt", FileType.txt, "a", "a", "a");
     }
-  }
-
-  Widget _panel() {
-    return Padding(
-      padding: EdgeInsets.fromLTRB(15.0, 12.0, 15.0, 0),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Container(
-                width: 35,
-                height: 5,
-                decoration: BoxDecoration(
-                    color: Colors.grey[300],
-                    borderRadius: BorderRadius.all(Radius.circular(12.0))),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 20.0,
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                child: Text(
-                  'Terminal',
-                  style: Type.header4.apply(color: Colors.black),
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 15.0,
-          ),
-          Expanded(
-            child: TerminalPage(),
-          ),
-        ],
-      ),
-    );
-    // TODO: make SFTP panel
   }
 
   Widget _body(context) {
@@ -117,7 +72,7 @@ class FileEditPage extends StatelessWidget {
         minHeight: 0,
         maxHeight: MediaQuery.of(context).size.height -
             MediaQuery.of(context).padding.top,
-        panel: _panel(),
+        panel: EditorPanel(),
         body: _body(context),
       ),
     );
